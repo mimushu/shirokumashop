fcaaff415349613588300b5b92b60ea32575b6a0
ShuMimura <shu.mimura@plaid.co.jp>
2025-05-13 18:54:22 +0900

    add dummy products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb に追記
+10.times do |i|
+  Product.create!(
+    name: "サンプル商品#{i + 1}",
+    price: (i + 1) * 400,
+    stock: 20,
+    description: "テスト用の商品説明です"
+  )
+end
+rails db:seed
+git add db/seeds.rb
+git commit -m "seed: add sample products"
+git push
+
