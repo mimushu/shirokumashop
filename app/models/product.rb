c9f514c9fd3f205e13fba419442ad6283ad71f30
ShuMimura <shu.mimura@plaid.co.jp>
2025-05-13 19:18:37 +0900

    chore: allowlist searchable attributes for Ransack

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  # これを追加 ----------------------------
+  def self.ransackable_attributes(auth_object = nil)
+    %w[id name description price stock created_at updated_at]
+  end
+  # --------------------------------------
 end
